24b6a66326094ca57d2cbb64ffa58ddd23a76002
sina <sadeghesfahani.sina@gmail.com>
2025-03-20 10:03:56 +0100

    Add User app to the project
    Introduce a new `user` app by adding `UserConfig` to the settings. Created boilerplate files for models, admin, views, and tests to establish the app's structure. No functional code is included yet, as this is the initial setup.

--- erp/settings.py
+++ erp/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'invoice.apps.InvoiceConfig'
+    'invoice.apps.InvoiceConfig',
+    'user.apps.UserConfig'
 ]
 
 MIDDLEWARE = [
